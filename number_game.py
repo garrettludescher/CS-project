1f5f97470705a749d566f5211ac3120add267e5c
Garrett Ludescher <42181813+garrettludescher@users.noreply.github.com>
2019-07-12 10:38:23 -0400

    Create number_game.py

---
+++ number_game.py
@@ -0,0 +1,14 @@
+x = 0
+'''
+while x <6:
+   print(x)
+   x = x+1
+'''
+y = 0
+while int(y) != 6:
+   y = input('Please type the number 6.')
+   print('You typed ', y)
+   print()
+
+print('Finally.')
+
